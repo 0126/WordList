a7c96648ec4046eac4e83cc7b5cf1c3e0b1cf585
Yoshiki Kishimoto <0126yoshiki@icloud.com>
2018-04-14 01:30:20 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by Yoshiki Kishimoto on 2018/04/11.
+//  Copyright © 2018年 Yoshiki Kishimoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func back(segue: UIStoryboardSegue) {
+    }
+    
+    @IBAction func starButtonTapped() {
+        let saveData = UserDefaults.standard
+        
+        if let wordArray = saveData.array(forKey: "WORD") {
+            if wordArray.count > 0 {
+                self.performSegue(withIdentifier: "toQuestionView", sender: nil)
+            }
+        }
+        
+        let alert: UIAlertController = UIAlertController(
+            title: "単語",
+            message: "まずは「単語一覧」をタップして単語登録してください。",
+            preferredStyle: .alert
+        )
+        
+        alert.addAction(UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil
+        ))
+        
+        self.present(alert, animated: true, completion:nil)
+        
+     }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
